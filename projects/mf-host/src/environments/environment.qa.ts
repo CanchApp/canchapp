24a42d1cc2dfe1cc3dbbbb8071b88d1981cb7424
manuel zambrano <manuelzambrano@outlook.com>
2025-04-13 10:51:37 -0500

    v1 deploy

---
+++ projects/mf-host/src/environments/environment.qa.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    remotes: {
+        mfBooking:    "mf-booking/remoteEntry.js",
+        mfCatalog:    "mf-catalog/remoteEntry.js",
+        mfCompany:    "mf-company/remoteEntry.js",
+        mfDashBoard:  "mf-dashboard/remoteEntry.js",
+        mfReport:     "mf-report/remoteEntry.js",
+        mfUser:       "mf-user/remoteEntry.js",        
+      },
+    apiUrl: 'http://my-dev-url'
+};
