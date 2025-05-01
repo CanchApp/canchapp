5a45e76f8b191cb2d55e7822beb7517efa8c378a
manuel zambrano <manuelzambrano@outlook.com>
2025-05-01 10:49:06 -0500

    fix the save when create a new court

--- projects/mf-catalog/src/app/models/courtRate.model.ts
+++ projects/mf-catalog/src/app/models/courtRate.model.ts
@@ -2,7 +2,7 @@
 export class CourtRateDTO {
     idCourtRate: number | undefined | string;
     day: number | undefined;
-    price: number | undefined;
+    value: number | undefined;
     timeStart: string | undefined;
     timeEnd: string | undefined;
 }
