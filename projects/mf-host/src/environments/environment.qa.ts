cccbae6ae65f8d08622be18346b718ff5685b2d9
manuel zambrano <manuelzambrano@outlook.com>
2025-05-04 10:41:59 -0500

    revert url

--- projects/mf-host/src/environments/environment.qa.ts
+++ projects/mf-host/src/environments/environment.qa.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: false,
     remotes: {
-      mfBooking:    "mfBooking@https://happy-hill-058471710.6.azurestaticapps.net/mf-booking/remoteEntry.js",
-      mfCatalog:    "mfCatalog@https://happy-hill-058471710.6.azurestaticapps.net/mf-catalog/remoteEntry.js",
-      mfCompany:    "mfCompany@https://happy-hill-058471710.6.azurestaticapps.net/mf-company/remoteEntry.js",
-      mfDashBoard:  "mfDashBoard@https://happy-hill-058471710.6.azurestaticapps.net/mf-dashboard/remoteEntry.js",
-      mfReport:     "mfReport@https://happy-hill-058471710.6.azurestaticapps.net/mf-report/remoteEntry.js",
-      mfUser:       "mfUser@https://happy-hill-058471710.6.azurestaticapps.net/mf-user/remoteEntry.js"     
+      mfBooking:    "https://happy-hill-058471710.6.azurestaticapps.net/mf-booking/remoteEntry.js",
+      mfCatalog:    "https://happy-hill-058471710.6.azurestaticapps.net/mf-catalog/remoteEntry.js",
+      mfCompany:    "https://happy-hill-058471710.6.azurestaticapps.net/mf-company/remoteEntry.js",
+      mfDashBoard:  "https://happy-hill-058471710.6.azurestaticapps.net/mf-dashboard/remoteEntry.js",
+      mfReport:     "https://happy-hill-058471710.6.azurestaticapps.net/mf-report/remoteEntry.js",
+      mfUser:       "https://happy-hill-058471710.6.azurestaticapps.net/mf-user/remoteEntry.js"     
     },
     apiUrl: 'http://my-dev-url'
 };
