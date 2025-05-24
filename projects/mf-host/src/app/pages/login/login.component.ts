1ff5bc6c38869fdadf747c4e38d6bbbd980119e2
Cesar Fernandez <cesarcharri08@gmail.com>
2025-05-24 12:55:47 -0500

    feat: scroll on booking section, loading spinner & alert message at login page

--- projects/mf-host/src/app/pages/login/login.component.ts
+++ projects/mf-host/src/app/pages/login/login.component.ts
@@ -3,6 +3,12 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { TranslateService } from '@ngx-translate/core';
 import { AuthService } from '../../services/auth.service';
 import { AuthCredentials } from '../../models/login';
+import {
+  NotificationService,
+  NotificationTypeEnum,
+  PlacementAlignEnum,
+  PlacementFromEnum
+} from 'commons-lib';
 
 @Component({
   selector: 'login',
@@ -12,11 +18,14 @@ import { AuthCredentials } from '../../models/login';
 export class LoginComponent {
 
   loginForm: FormGroup;
+  isLoading: boolean = false;
 
   constructor(
     public translate: TranslateService,
     private readonly fb: FormBuilder, 
-    private readonly authService: AuthService) {
+    private readonly authService: AuthService,
+    private readonly notificationService: NotificationService
+  ) {
       this.loginForm = this.fb.group({
         email: ['', [Validators.required, Validators.email]],
         password: ['', Validators.required]
@@ -25,10 +34,29 @@ export class LoginComponent {
 
   onClicklogin() {
     if (this.loginForm.valid) {
+      this.isLoading = true;
+
       const credentials: AuthCredentials = this.loginForm.value;
       this.authService.Login(credentials).subscribe(
-        () => console.log('Login exitoso'),
-        (error) => console.error('Error en la autenticación:', error)
+        () => {
+          this.isLoading = false;
+          console.log('Login exitoso');
+        },
+        (error) => {
+          this.isLoading = false;
+          
+          this.notificationService.showCustomNotification({
+            delay: 1000,
+            title: this.translate.instant('Login.InvalidCredentialsTitle'),
+            message: this.translate.instant('Login.InvalidCredentialsMsg'),
+            icon: 'fa-solid fa-exclamation',
+            type: NotificationTypeEnum.Warning,
+            placementFrom: PlacementFromEnum.Bottom,
+            placementAlign: PlacementAlignEnum.Center,
+          });
+
+          console.error('Error en la autenticación:', error);
+        }
       );
     }
   }
